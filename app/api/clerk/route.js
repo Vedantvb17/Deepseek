5b9c0c70bfa32ba256ce7acfe6ce3bb55088985d
Vedant Barde <vedantvb17@gmail.com>
2025-07-01 21:14:10 +0530

    route js updated

--- app/api/clerk/route.js
+++ app/api/clerk/route.js
@@ -2,11 +2,11 @@ import { Webhook } from "svix";
 import connectDB from "@/config/db";
 import User from "@/models/User";
 import { headers } from "next/headers";
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
 export async function POST(req){
-    const wh = new Webhook(process.env.SIGNING_SECRET);
-    const headerPayload = await headers();
+    const wh = new Webhook(process.env.SIGNING_SECRET)
+    const headerPayload = headers()
     const svixHeaders = {
         "svix-id": headerPayload.get("svix-id"),
         "svix-signature": headerPayload.get("svix-signature"),
@@ -41,7 +41,10 @@ export async function POST(req){
 
         case 'user.deleted' :
             await User.findByIdAndDelete(data.id)
-            break;    
+            break;   
+            
+        default:
+            break;
     }
-    return NextRequest.json({message: "Event Received"});
+    return NextResponse.json({message: "Event Received"});
 }
